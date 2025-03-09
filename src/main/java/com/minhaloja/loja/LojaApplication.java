13ed002f14fa2c30b353f0fd1a2f77e3ab178b66
Rodrigo Garcia Buzato <rodrigo.buzato@sensedia.com>
2025-03-09 10:04:39 -0300

    Implementação inicial do health check
    Implementação inicial do health check

--- src/main/java/com/minhaloja/loja/LojaApplication.java
+++ src/main/java/com/minhaloja/loja/LojaApplication.java
@@ -2,8 +2,9 @@ package com.minhaloja.loja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class LojaApplication {
 
 	public static void main(String[] args) {
